69f7644a55961e16fa30a44cba983aaf92ea9462
Rafael Franco <rafaelfr@outlook.com>
2019-05-07 19:35:09 -0300

    chore: change primary and link color from docz
    We were using a hex color and we decided to use a web safe color
    instead.

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,8 @@ export default {
   menu: ['Getting Started', 'Components'],
   themeConfig: {
     colors: {
-      primary: '#5f4b8b'
+      primary: 'salmon',
+      link: 'salmon',
     }
   }
 }
